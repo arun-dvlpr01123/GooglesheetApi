90c8700889bcf078bb6f193944695d1698ca8a06
arun-dvlpr01123 <arun.dvlpr01123@gmail.com>
2021-07-12 21:31:50 -0500

    Added Array Sort program

---
+++ array_sort.py
@@ -0,0 +1,15 @@
+
+
+my_array = [8, 9, 52, 98, 71, 85, 23, 1, 10, 11, 23, 24]
+
+array_len = len(my_array)
+
+for i in range(0,array_len):
+    for j in range(i+1,array_len):
+        if my_array[i] >= my_array[j]:
+            temp = my_array[i]
+            my_array[i] = my_array[j]
+            my_array[j] = temp
+
+
+print(my_array)
